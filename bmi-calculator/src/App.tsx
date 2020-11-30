37be9f63af70fe6b17887a87e720a2f32f466fc6
Cody Silverglat <cody.silverglat@Cody-Silverglats-MacBook.local>
2020-11-29 18:41:33 -0600

    replace vanilla js with react

---
+++ bmi-calculator/src/App.tsx
@@ -0,0 +1,119 @@
+import React, {useRef, useState} from "react";
+import {
+  IonApp,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+
+import {calculatorOutline, refreshOutline} from "ionicons/icons";
+
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+
+/* Theme variables */
+import "./theme/variables.css";
+
+const App: React.FC = () => {
+  const [calculatedBmi, setCalculatedBmi] = useState<number>();
+
+  const weightInputRef = useRef<HTMLIonInputElement>(null);
+  const heightInputRef = useRef<HTMLIonInputElement>(null);
+
+  const calculateBMI = () => {
+    const enteredWeight = weightInputRef.current!.value;
+    const enteredHeight = heightInputRef.current!.value;
+
+    if (!enteredHeight || !enteredWeight) {
+      return;
+    }
+
+    const bmi = +enteredWeight / (+enteredHeight * +enteredHeight);
+
+    setCalculatedBmi(bmi);
+  };
+
+  const resetInputs = () => {
+    weightInputRef.current!.value = "";
+    heightInputRef.current!.value = "";
+  };
+
+  return (
+    <IonApp>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>BMI Calculator</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className='ion-padding'>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position='floating'>Your Height</IonLabel>
+                <IonInput ref={heightInputRef}></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position='floating'>Your Weight</IonLabel>
+                <IonInput ref={weightInputRef}></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol className='ion-text-left'>
+              <IonButton onClick={calculateBMI}>
+                <IonIcon icon={calculatorOutline} slot='start' />
+                Calculate
+              </IonButton>
+            </IonCol>
+            <IonCol className='ion-text-right'>
+              <IonButton onClick={resetInputs}>
+                <IonIcon icon={refreshOutline} slot='start' />
+                Reset
+              </IonButton>
+            </IonCol>
+          </IonRow>
+          {calculatedBmi && (
+            <IonRow>
+              <IonCard>
+                <IonCardContent>
+                  <h2>{calculatedBmi}</h2>
+                </IonCardContent>
+              </IonCard>
+            </IonRow>
+          )}
+        </IonGrid>
+      </IonContent>
+    </IonApp>
+  );
+};
+
+export default App;
